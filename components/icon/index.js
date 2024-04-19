c86da81680f4c54728d3742c07c26176c6a168f4
Qinyubin <qin.yubin@mobcb.com>
2024-04-19 13:56:07 +0800

    swich组件

--- components/icon/index.js
+++ components/icon/index.js
@@ -1,3 +1,13 @@
+function controller($scope){
+    this.$postLink = function (){
+        console.log($scope.$id)
+        $scope.$on('changeStyle', function(e){
+            console.log(e)
+        })
+    }
+}
+
+
 app.component('mobIcon', {
     transclude: true,
     templateUrl: './components/icon/mob-icon.html',
@@ -9,28 +19,41 @@ app.component('mobIcon', {
 
     .component('mobIconDownArrow', {
         templateUrl: './components/icon/downArrow.html',
+        controller:controller
     })
     .component('mobIconCircleClose', {
         templateUrl: './components/icon/circleClose.html',
+        controller:controller
     })
     .component('mobIconClose', {
         templateUrl: './components/icon/close.html',
+        controller:controller
+    })
+    .component('mobIconCheck', {
+        templateUrl: './components/icon/check.html',
+        controller:controller
     })
     .component('mobIconVisible', {
         templateUrl: './components/icon/visible.html',
+        controller:controller
     })
     .component('mobIconInvisible', {
         templateUrl: './components/icon/invisible.html',
+        controller:controller
     })
     .component('mobIconCalendar', {
         templateUrl: './components/icon/calendar.html',
+        controller:controller
     })
     .component('mobIconSearch', {
         templateUrl: './components/icon/search.html',
+        controller:controller
     })
     .component('mobIconIncrease', {
         templateUrl: './components/icon/increase.html',
+        controller:controller
     })
     .component('mobIconDecrease', {
         templateUrl: './components/icon/decrease.html',
+        controller:controller
     })
--- components/switch/index.js
+++ components/switch/index.js
@@ -1,12 +1,11 @@
-function controller($scope, $element, uuId, $transclude) {
+function controller($scope, $element, $compile, $transclude) {
     const _that = this
-    Object.assign(_that, {
-        activeColor: '#13ce66',
-        inactiveColor: '#ff4949',
-    })
     // 初始化工作
     this.$onInit = function () {
-
+        Object.assign(_that, {
+            activeColor: _that.activeColor ? _that.activeColor : '#13ce66',
+            inactiveColor: _that.inactiveColor ? _that.inactiveColor : '#ff4949',
+        })
     }
 
     this.$onChanges = function (changes) {
@@ -16,13 +15,21 @@ function controller($scope, $element, uuId, $transclude) {
     }
 
     this.$postLink = function () {
-        this.ngModel ? this.setActiveStyle() : this.setInActiveStyle()
+        this.ngModel ? this.setActiveChange() : this.setInActiveChange()
         // 初始化监听事件
         initWatcher()
+        if (this.showInactiveIcon()) {
+            $element[0].querySelector('.mob-switch__inactive_icon').appendChild($compile(`<${_that.inactiveIcon}></${_that.inactiveIcon}>`)($scope)[0])
+        }
+        if (this.showActiveIcon()) {
+            $element[0].querySelector('.mob-switch__active_icon').appendChild($compile(`<${_that.activeIcon}></${_that.activeIcon}>`)($scope)[0])
+        }
     }
 
-    function initWatcher(){
-        $scope.$watch(()=>{return _that.ngModel},function(v){
+    function initWatcher() {
+        $scope.$watch(() => {
+            return _that.ngModel
+        }, function (v) {
             _that.changeHandle()
         })
     }
@@ -30,24 +37,86 @@ function controller($scope, $element, uuId, $transclude) {
     /**
      * 设置激活时样式
      */
-    this.setActiveStyle = function () {
+    this.setActiveChange = function () {
         let core = $element[0].querySelector('.mob-switch-core')
         Object.assign(core.style, {
             backgroundColor: _that.activeColor,
             borderColor: _that.activeColor,
         })
+        if (this.inlinePrompt) {
+            let inner = core.querySelector('.mob-switch__inner')
+            if (!this.isInlinePromptText()) {
+                while (inner.firstChild) {
+                    inner.removeChild(inner.firstChild)
+                }
+                inner.appendChild($compile(`<${_that.activeIcon}></${_that.activeIcon}>`)($scope)[0])
+            }
+        }
 
     }
     /**
      * 设置未激活时样式
      */
-    this.setInActiveStyle = function () {
+    this.setInActiveChange = function () {
         let core = $element[0].querySelector('.mob-switch-core')
         Object.assign(core.style, {
             backgroundColor: _that.inactiveColor,
             borderColor: _that.inactiveColor,
         })
+
+        if (this.inlinePrompt) {
+            let inner = core.querySelector('.mob-switch__inner')
+            if (!this.isInlinePromptText()) {
+                while (inner.firstChild) {
+                    inner.removeChild(inner.firstChild)
+                }
+                inner.appendChild($compile(`<${_that.inactiveIcon}></${_that.inactiveIcon}>`)($scope)[0])
+            }
+        }
     }
+
+    /**
+     * 内部文字
+     */
+    this.inlinePromptText = function () {
+        if (this.ngModel) {
+            return this.activeText
+        } else {
+            return this.inactiveText
+        }
+    }
+
+    this.isInlinePromptText = function () {
+        return !this.activeIcon && !this.inactiveIcon
+    }
+
+    /**
+     * 是否显示激活文字提示
+     */
+    this.showActiveText = function () {
+        return !this.inlinePrompt && !this.activeIcon && this.activeText
+    }
+
+    /**
+     * 是否显示未激活文字提示
+     */
+    this.showInactiveText = function () {
+        return !this.inlinePrompt && !this.activeIcon && this.inactiveText
+    }
+
+    /**
+     * 是否显示激活文字提示
+     */
+    this.showActiveIcon = function () {
+        return !this.inlinePrompt && this.activeIcon && !this.activeText
+    }
+    /**
+     * 是否显示未激活文字提示
+     */
+    this.showInactiveIcon = function () {
+        return !this.inlinePrompt && this.inactiveIcon && !this.inactiveText
+    }
+
 
     /**
      * 点击事件
@@ -58,11 +127,11 @@ function controller($scope, $element, uuId, $transclude) {
     /**
      * ngModel改变时
      */
-    this.changeHandle = function (){
+    this.changeHandle = function () {
         if (this.ngModel) {
-            this.setActiveStyle()
+            this.setActiveChange()
         } else {
-            this.setInActiveStyle()
+            this.setInActiveChange()
         }
     }
 }
@@ -73,12 +142,15 @@ app
         bindings: {
             ngModel: '=?',
             ngDisabled: '<?',
-            name:'<?',
+            name: '<?',
             activeColor: '<?',
             activeText: '<?',
+            activeIcon: '<?',
             activeValue: '<?',
             inactiveColor: '<?',
+            inactiveIcon: '<?',
             inactiveText: '<?',
+            inlinePrompt: '<?'
         },
         controller: controller
     })
