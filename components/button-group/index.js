d0b5051de04dae184c440d8935f03b54a9ff259a
Qinyubin <qin.yubin@mobcb.com>
2024-04-24 15:01:02 +0800

    buttonGroup

---
+++ components/button-group/index.js
@@ -0,0 +1,30 @@
+function controller($scope, $element, $transclude,$attrs, slot, attrHelp) {
+    const _that = this
+    // 初始化工作
+    this.$onInit = function () {
+        let abbParams = ['plain','round','circle','ngDisabled']
+        attrHelp.abbAttrsTransfer(this, abbParams, $attrs)
+    }
+
+
+    this.$onChanges = function (changes) {
+    }
+
+    this.$onDestroy = function () {
+    }
+
+
+    this.$postLink = function () {
+        if (this.icon) {
+            slot.appendChild($scope, $element[0], {icon:`<${_that.icon}></${_that.icon}>`})
+        }
+    }
+}
+
+app
+    .component('mobButtonGroup', {
+        transclude: true,
+        templateUrl: './components/button-group/mob-button-group.html',
+        bindings: { },
+        controller: controller
+    })
