49386ba262ebe73737c5fb16657c8149ef0ec8a0
Qinyubin <qin.yubin@mobcb.com>
2024-07-29 15:48:23 +0800

    feat 增加ICON

--- components/icon/index.js
+++ components/icon/index.js
@@ -10,68 +10,75 @@ app.component('mobIcon', {
     }
 })
 
-
-    .component('mobIconDownArrow', {
-        templateUrl: './components/icon/downArrow.html',
-        controller:controller
-    })
-    .component('mobIconCircleClose', {
-        templateUrl: './components/icon/circleClose.html',
-        controller:controller
-    })
-    .component('mobIconClose', {
-        templateUrl: './components/icon/close.html',
-        controller:controller
-    })
-    .component('mobIconCheck', {
-        templateUrl: './components/icon/check.html',
-        controller:controller
-    })
-    .component('mobIconVisible', {
-        templateUrl: './components/icon/visible.html',
-        controller:controller
-    })
-    .component('mobIconInvisible', {
-        templateUrl: './components/icon/invisible.html',
-        controller:controller
-    })
     .component('mobIconCalendar', {
         templateUrl: './components/icon/calendar.html',
         controller:controller
     })
-    .component('mobIconSearch', {
-        templateUrl: './components/icon/search.html',
-        controller:controller
-    })
-    .component('mobIconIncrease', {
-        templateUrl: './components/icon/increase.html',
-        controller:controller
-    })
-    .component('mobIconDecrease', {
-        templateUrl: './components/icon/decrease.html',
-        controller:controller
-    })
-    .component('mobIconLoading', {
-        templateUrl: './components/icon/loading.html',
-        controller:controller
-    })
-    .component('mobIconClock', {
-        templateUrl: './components/icon/clock.html',
+    .component('mobIconCaretBottom', {
+        templateUrl: './components/icon/caretBottom.html',
         controller:controller
     })
     .component('mobIconCaretLeft', {
         templateUrl: './components/icon/caretLeft.html',
         controller:controller
     })
-    .component('mobIconCaretTop', {
-        templateUrl: './components/icon/caretTop.html',
-        controller:controller
-    })
     .component('mobIconCaretRight', {
         templateUrl: './components/icon/caretRight.html',
         controller:controller
     })
-    .component('mobIconCaretBottom', {
-        templateUrl: './components/icon/caretBottom.html',
+    .component('mobIconCaretTop', {
+        templateUrl: './components/icon/caretTop.html',
+        controller:controller
+    })
+    .component('mobIconCheck', {
+        templateUrl: './components/icon/check.html',
+        controller:controller
+    })
+    .component('mobIconCircleClose', {
+        templateUrl: './components/icon/circleClose.html',
+        controller:controller
+    })
+    .component('mobIconClock', {
+        templateUrl: './components/icon/clock.html',
+        controller:controller
+    })
+    .component('mobIconClose', {
+        templateUrl: './components/icon/close.html',
+        controller:controller
+    })
+    .component('mobIconDecrease', {
+        templateUrl: './components/icon/decrease.html',
+        controller:controller
+    })
+    .component('mobIconDoubleArrowLeft', {
+        templateUrl: './components/icon/doubleArrowLeft.html',
+        controller:controller
+    })
+    .component('mobIconDoubleArrowRight', {
+        templateUrl: './components/icon/doubleArrowRight.html',
+        controller:controller
+    })
+    .component('mobIconDownArrow', {
+        templateUrl: './components/icon/downArrow.html',
+        controller:controller
+    })
+    .component('mobIconIncrease', {
+        templateUrl: './components/icon/increase.html',
+        controller:controller
+    })
+    .component('mobIconInvisible', {
+        templateUrl: './components/icon/invisible.html',
+        controller:controller
+    })
+    .component('mobIconLoading', {
+        templateUrl: './components/icon/loading.html',
+        controller:controller
+    })
+    .component('mobIconSearch', {
+        templateUrl: './components/icon/search.html',
+        controller:controller
+    })
+    .component('mobIconVisible', {
+        templateUrl: './components/icon/visible.html',
         controller:controller
     })
