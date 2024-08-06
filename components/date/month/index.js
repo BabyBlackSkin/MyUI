37f3a5c7fcf49174ae4fe041aff268c51a23e829
BabyBlackSkin <505627251@qq.com>
2024-08-07 00:12:51 +0800

    feat date,year,month拆开

---
+++ components/date/month/index.js
@@ -0,0 +1,136 @@
+
+function monthController($scope, $element, $attrs) {
+    const _that = this
+    // 初始化工作
+    this.$onInit = function () {
+        // 年份选择弹框是否显示控制
+        this.yearDatePickerDisplay = false;
+        // 当前年份
+        // this.$dayjs = dayjs()
+
+        $scope.$dayjs = dayjs()
+        // 当前月份
+        $scope.currentMonth = $scope.$dayjs.month() + 1
+
+        // 选择年份
+        $scope.year = $scope.$dayjs.year()
+        // 选择月份
+        $scope.month = $scope.currentMonth;
+
+        $scope.options = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+        ]
+    }
+
+
+    this.$onChanges = function (changes) {
+
+    }
+
+    this.$postLink = function () {
+        initWatcher()
+    }
+
+    this.$onDestroy = function () {
+    }
+
+
+    function initWatcher() {
+        $scope.$watchCollection(() => {
+            return _that.ngModel
+        }, function (newValue, oldValue) {
+            if (!newValue && !oldValue) {
+                return
+            }
+            // if (newValue === oldValue) {
+            //     return;
+            // }
+            if (angular.isDefined($attrs.change)) {
+                let opt = {value: newValue, attachment: this.attachment}
+                _that.change({opt: opt})
+            }
+            // ngModel改变时，获取年份
+            $scope.year = Number(newValue.split("-")[0])
+            // ngModel改变时，获取月份
+            $scope.month = Number(newValue.split("-")[1])
+        })
+    }
+
+    // 增加年份
+    this.increase = function () {
+        let newTime = dayjs(`${$scope.year}`, "YYYY").add(1, 'year')
+        $scope.year = newTime.year()
+        $scope.tempValue = angular.isUndefined($scope.tempValue) ? 1 : $scope.tempValue - 1
+        console.log($scope.year)
+        this.panelChangeHandle()
+    }
+
+    // 减少年份
+    this.decrease = function () {
+        let newTime = dayjs(`${$scope.year}`, "YYYY").subtract(1, 'year')
+        $scope.year = newTime.year()
+        $scope.tempValue = angular.isUndefined($scope.tempValue) ? 1 : $scope.tempValue - 1
+        console.log($scope.year)
+        this.panelChangeHandle()
+    }
+
+
+    // 日历所选日期变更
+    this.calendarChangeHandle = function () {
+        if (angular.isDefined($attrs.calendarChange)) {
+            let opt = {value: this.ngModel, attachment: this.attachment}
+            _that.calendarChange({opt: opt})
+        }
+    }
+
+    // 日历所选日期变更 TODO
+    this.panelChangeHandle = function () {
+        if (angular.isDefined($attrs.panelChange)) {
+            let opt = {value: this.ngModel, attachment: this.attachment}
+            _that.panelChange({opt: opt})
+        }
+    }
+
+    // 选择月份
+    this.select = function (month) {
+        $scope.month = month
+        this.ngModel = $scope.year + "-" + month
+        this.calendarChangeHandle()
+    }
+
+    // 改变年份
+    this.changeYear = function (opt) {
+        $scope.year = opt.value
+    }
+
+    this.hideYearDatePicker = function () {
+        this.yearDatePickerDisplay = false
+    }
+
+    // 显示年份选择框
+    this.showYearDatePicker = function () {
+        this.yearDatePickerDisplay = true
+    }
+
+
+}
+
+app
+    .component('mobDateMonth', {
+        transclude: true,
+        templateUrl: function ($element, $attrs) {
+            return `./components/date/month/index.html`
+        },
+        bindings: {
+            ngModel: '=?',
+            type: "<?",// 选择器类型：year
+            attachment: "<?",
+            change: "&?",
+            calendarChange: "&?",
+            panelChange: "&?",
+            disabledDate: "&?", // 日期是否可选，入参：日期（目前仅支持在类型为date时启用）
+        },
+        controller: monthController
+    })
---
+++ components/date/year/index.js
@@ -0,0 +1,141 @@
+function yearController($scope, $element, $attrs) {
+    const _that = this
+    // 初始化工作
+    this.$onInit = function () {
+        // 当前年份
+        $scope.$dayjs = dayjs()
+        // 当前年份
+        $scope.currentYear = $scope.$dayjs.year()
+
+        // 获取个位数（只显示近10年的月份）
+        let singleDigit = $scope.currentYear % 100 % 10;
+        $scope.startYear = $scope.currentYear - singleDigit
+        $scope.endYear = $scope.startYear + 9
+
+        this.renderOptions()
+
+    }
+
+
+    this.$onChanges = function (changes) {
+
+    }
+
+
+    this.$postLink = function () {
+        initWatcher();
+    }
+
+    this.$onDestroy = function () {
+    }
+
+    function initWatcher() {
+        $scope.$watchCollection(() => {
+            return _that.ngModel
+        }, function (newValue, oldValue) {
+            if (!newValue && !oldValue) {
+                return
+            }
+            if (newValue === oldValue) {
+                return;
+            }
+            if (angular.isDefined($attrs.change)) {
+                let opt = {value: newValue, attachment: this.attachment}
+                _that.change({opt: opt})
+            }
+
+            // ngModel发生变化时，重新计算startYear和endYear，并重新readerOptions
+            // 获取最后一位
+            let timeScope = _that.getStartYearAndEndYear(newValue)
+            if ($scope.startYear !== timeScope[0] && $scope.endYear !== timeScope[1]) {
+                $scope.startYear = timeScope[0]
+                $scope.endYear = timeScope[1]
+                _that.renderOptions()
+            }
+        })
+    }
+
+    // 根据value计算年份范围
+    this.getStartYearAndEndYear = function (value) {
+        let lastDigitYear = value % 10
+        let startYear = value - lastDigitYear
+        let endYear = startYear + 9
+        return [startYear, endYear]
+    }
+
+
+    // 根据年份计算选项
+    this.renderOptions = function () {
+        let yearGroupInx = 0
+        $scope.options = []
+        for (let i = $scope.startYear; i <= $scope.endYear; i++) {
+            if (!$scope.options[yearGroupInx]) {
+                $scope.options[yearGroupInx] = []
+            } else if ($scope.options[yearGroupInx].length === 4) {
+                yearGroupInx++
+                $scope.options[yearGroupInx] = []
+            }
+            $scope.options[yearGroupInx].push(i)
+        }
+    }
+
+    // 增加年份
+    this.increase = function () {
+        $scope.startYear = $scope.startYear + 10
+        $scope.endYear = $scope.startYear + 9
+        this.renderOptions()
+        this.panelChangeHandle()
+    }
+
+    // 减少年份
+    this.decrease = function () {
+        $scope.startYear = $scope.startYear - 10
+        $scope.endYear = $scope.startYear + 9
+        this.renderOptions()
+        this.panelChangeHandle()
+    }
+
+    // 日历所选日期变更
+    this.calendarChangeHandle = function () {
+        if (angular.isDefined($attrs.calendarChange)) {
+            let opt = {value: this.ngModel, attachment: this.attachment}
+            _that.calendarChange({opt: opt})
+        }
+    }
+
+    // 日历面板变更
+    this.panelChangeHandle = function () {
+        if (angular.isDefined($attrs.panelChange)) {
+            let opt = {value: this.ngModel, attachment: this.attachment}
+            _that.panelChange({opt: opt})
+        }
+    }
+
+
+    // 日历项被点击时触发
+    // 选择年份
+    this.select = function (year) {
+        this.ngModel = year
+        this.calendarChangeHandle()
+    }
+
+}
+
+
+app
+    .component('mobDateYear', {
+        transclude: true,
+        templateUrl: function ($element, $attrs) {
+            return `./components/date/year/index.html`
+        },
+        bindings: {
+            ngModel: '=?',
+            type: "<?",// 选择器类型：year
+            attachment: "<?",
+            change: "&?",
+            calendarChange: "&?",
+            panelChange: "&?",
+            disabledDate: "&?", // 日期是否可选，入参：日期（目前仅支持在类型为date时启用）
+        },
+        controller: yearController
+    })
